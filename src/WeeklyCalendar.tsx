f3f19105358d1c01cc5467d79fc84e3c01b8b8b4
fraulueneburg <github@fraulueneburg.de>
2025-05-10 12:12:58 +0200

    added month name(s), styled weekends

--- src/WeeklyCalendar.tsx
+++ src/WeeklyCalendar.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import { addDays, format, getISOWeek, subDays, subWeeks } from 'date-fns'
+import { endOfWeek, format, getISOWeek, startOfWeek, subDays, subWeeks } from 'date-fns'
 
 export default function WeeklyCalendar() {
-	const today = new Date()
 	const [offset, setOffset] = useState(0)
 
 	const weekdaysArrUS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
@@ -11,19 +10,21 @@ export default function WeeklyCalendar() {
 	const weekdaysArr = weekStartsOnMonday ? weekdaysArrEU : weekdaysArrUS
 	const timePeriodInDays = 7
 
-	const ISOweekNum = getISOWeek(subWeeks(new Date(today), offset * -1))
-
+	const today = new Date()
 	const currWeekdayNum = weekStartsOnMonday ? subDays(new Date(today), 1).getDay() : today.getDay()
-	const currDay = today.getDate()
-	const currMonth = today.getMonth()
-	const currYear = today.getFullYear()
-	const currDayName = format(new Date(today), 'ccc')
 
-	const currMonthName = format(new Date(today), 'MMM')
+	const refDate = subDays(new Date(today), offset * timePeriodInDays * -1)
+	const firstDayOfWeek = startOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
+	const lastDayOfWeek = endOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
+	const firstMonth = format(new Date(firstDayOfWeek), 'MMM')
+	const lastMonth = format(new Date(lastDayOfWeek), 'MMM')
+
+	const writtenMonthName = firstMonth === lastMonth ? `${firstMonth}` : `${firstMonth}/${lastMonth}`
+	const ISOweekNum = getISOWeek(subWeeks(new Date(today), offset * -1))
 
 	return (
 		<>
-			<h1>{currMonthName}</h1>
+			<h1>{writtenMonthName}</h1>
 			<button onClick={() => setOffset((prev) => prev - 1)}>prev</button>
 			<button onClick={() => setOffset(0)} disabled={offset === 0}>
 				today
@@ -34,19 +35,18 @@ export default function WeeklyCalendar() {
 
 			<div className="week">
 				{weekdaysArr.map((dayName, index) => {
-					const dayDate = addDays(new Date(today), currWeekdayNum - index + offset * timePeriodInDays)
+					const dayDate = subDays(new Date(today), currWeekdayNum - index + timePeriodInDays * offset * -1)
 					const dayNum = dayDate.getDate()
 					const isToday = offset === 0 && currWeekdayNum === index
-					const classes = `day ${isToday ? ' today' : ''}`
+					const isWeekend = dayName === 'Sat' || dayName === 'Sun'
+					const classes = `day${isToday ? ' today' : ''}${isWeekend ? ' weekend' : ''}`
 
 					return (
-						<>
-							<div className={classes} key={dayName}>
-								<h3>
-									{dayName} {dayNum}
-								</h3>
-							</div>
-						</>
+						<div className={classes} key={`${offset}-${index}`}>
+							<h3>
+								{dayName} {dayNum}
+							</h3>
+						</div>
 					)
 				})}
 			</div>
