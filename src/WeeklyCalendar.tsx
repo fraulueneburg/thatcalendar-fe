75d8bfef8805f7d3b3dbb85dc99d209fef72eff4
fraulueneburg <github@fraulueneburg.de>
2025-05-10 14:08:39 +0200

    added display logic for years inside headline

--- src/WeeklyCalendar.tsx
+++ src/WeeklyCalendar.tsx
@@ -11,21 +11,46 @@ export default function WeeklyCalendar() {
 	const timePeriodInDays = 7
 
 	const today = new Date()
-	const currWeekdayNum = weekStartsOnMonday ? subDays(new Date(today), 1).getDay() : today.getDay()
+	const weekdayNumToday = weekStartsOnMonday ? subDays(new Date(today), 1).getDay() : today.getDay()
+	const yearToday = today.getFullYear()
 
 	const refDate = subDays(new Date(today), offset * timePeriodInDays * -1)
-	const firstDayOfWeek = startOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
-	const lastDayOfWeek = endOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
-	const firstMonth = format(new Date(firstDayOfWeek), 'MMM')
-	const lastMonth = format(new Date(lastDayOfWeek), 'MMM')
+	const startDay = startOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
+	const endDay = endOfWeek(new Date(refDate), { weekStartsOn: weekStartsOnMonday ? 1 : 0 })
+	const startMonth = format(new Date(startDay), 'MMM')
+	const endMonth = format(new Date(endDay), 'MMM')
+	const startYear = startDay.getFullYear()
+	const endYear = endDay.getFullYear()
+	const isSameMonth = startMonth === endMonth
+	const isTwoYears = startMonth === 'Dec' && endMonth === 'Jan'
+	const hideIfCurrYear = (year: number) => {
+		return year === yearToday ? null : <span className="year"> {year}</span>
+	}
 
-	const writtenMonthName = firstMonth === lastMonth ? `${firstMonth}` : `${firstMonth}/${lastMonth}`
+	const periodHeadline = isSameMonth ? (
+		<>
+			{startMonth}
+			{hideIfCurrYear(startYear)}
+		</>
+	) : isTwoYears ? (
+		<>
+			{startMonth}
+			{hideIfCurrYear(startYear)}/{endMonth}
+			{hideIfCurrYear(endYear)}
+		</>
+	) : (
+		<>
+			{startMonth}/{endMonth}
+			{hideIfCurrYear(endYear)}
+		</>
+	)
+
 	const ISOweekNum = getISOWeek(subWeeks(new Date(today), offset * -1))
 
 	return (
 		<>
 			<header className="page-header">
-				<h1>{writtenMonthName}</h1>
+				<h1>{periodHeadline}</h1>
 				<nav>
 					<button onClick={() => setOffset((prev) => prev - 1)}>prev</button>
 					<button onClick={() => setOffset(0)} disabled={offset === 0}>
@@ -41,9 +66,9 @@ export default function WeeklyCalendar() {
 					{ISOweekNum}
 				</h2>
 				{weekdaysArr.map((dayName, index) => {
-					const dayDate = subDays(new Date(today), currWeekdayNum - index + timePeriodInDays * offset * -1)
+					const dayDate = subDays(new Date(today), weekdayNumToday - index + timePeriodInDays * offset * -1)
 					const dayNum = dayDate.getDate()
-					const isToday = offset === 0 && currWeekdayNum === index
+					const isToday = offset === 0 && weekdayNumToday === index
 					const isWeekend = dayName === 'Sat' || dayName === 'Sun'
 					const classes = `day${isToday ? ' today' : ''}${isWeekend ? ' weekend' : ''}`
 
