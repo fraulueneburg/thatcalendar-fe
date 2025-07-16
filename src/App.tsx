61dd12906612d7c44e4df7b6ab99cd62bc308d5a
fraulueneburg <github@fraulueneburg.de>
2025-07-16 23:07:32 +0200

    refactor: uninstall radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-import '@radix-ui/themes/styles.css'
 import './styles/App.scss'
 import WeeklyCalendar from './components/WeeklyCalendar'
 import Header from './components/Header'
-import { Theme } from '@radix-ui/themes'
 
 function App() {
 	return (
 		<>
-			<Theme appearance="dark">
-				<Header />
-				<main>
-					<WeeklyCalendar />
-				</main>
-			</Theme>
+			<Header />
+			<main>
+				<WeeklyCalendar />
+			</main>
 		</>
 	)
 }
