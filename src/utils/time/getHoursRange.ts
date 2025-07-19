31e2f4da5acb66a036d89d746555e09ecd9b8fa2
fraulueneburg <github@fraulueneburg.de>
2025-07-19 11:18:05 +0200

    feature (hours range): account for end num being 0 (=24)

--- src/utils/time/getHoursRange.ts
+++ src/utils/time/getHoursRange.ts
@@ -1,2 +1,5 @@
-const getHoursRange = (start: number, end: number) => Array.from({ length: end - start + 1 }, (_, i) => start + i)
+const getHoursRange = (start: number, end: number): number[] => {
+	end = end === 0 ? 24 : end
+	return Array.from({ length: end - start + 1 }, (_, i) => start + i)
+}
 export { getHoursRange }
