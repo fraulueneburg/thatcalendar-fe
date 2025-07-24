8a5432572fb424cf00939266b6c6ec43be5dec29
fraulueneburg <github@fraulueneburg.de>
2025-07-24 08:43:19 +0200

    refactor (app): add context to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './styles/App.scss'
 import Calendar from './components/Calendar'
 import Header from './components/Header'
+import { DataContextWrapper } from './context/Data.context'
 
 function App() {
 	return (
 		<>
-			<Header />
-			<main>
-				<Calendar />
-			</main>
+			<DataContextWrapper>
+				<Header />
+				<main>
+					<Calendar />
+				</main>
+			</DataContextWrapper>
 		</>
 	)
 }
