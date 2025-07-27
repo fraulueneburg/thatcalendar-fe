b6af38473dfb0901e8e5e5020afcb78120555ee5
fraulueneburg <github@fraulueneburg.de>
2025-07-27 10:11:05 +0200

    feat: add new category form

--- src/components/Forms/NewCategoryForm.tsx
+++ src/components/Forms/NewCategoryForm.tsx
@@ -1,39 +1,65 @@
-import { categoryArr } from '../../data/dummydata'
-const mainCategories = categoryArr.filter((elem) => !elem.parent)
+import { useDataContext } from '../../context/Data.context'
+import { CategoryType } from '../../types'
+import CategoryTitleInput from './CategoryTitleInput'
 
 export default function NewCategoryForm() {
+	const { categoryData, setCategoryData } = useDataContext()
+	const mainCategories = categoryData.data.filter((elem) => !elem.parent)
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+
+		const form = event.currentTarget
+		const formData = new FormData(form)
+		const invalid = form.querySelectorAll('[aria-invalid="true"]').length > 0
+
+		if (invalid) {
+			const firstInvalid = form.querySelector('[aria-invalid="true"]')
+
+			if (firstInvalid && firstInvalid instanceof HTMLElement) {
+				firstInvalid.focus()
+			}
+			return
+		}
+
+		const customId = `c-${categoryData.data.length}`
+
+		const newCategory = { _id: customId, ...Object.fromEntries(formData.entries()) } as CategoryType
+		setCategoryData((prev) => ({ ...prev, data: [...prev.data, newCategory] }))
+
+		console.log(newCategory)
+	}
+
 	return (
 		<>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<p>
-					<label>
-						Name
-						<input type="text" required />
-					</label>
+					<CategoryTitleInput />
 				</p>
 				<p>
-					<label>
+					<label htmlFor="parent">
 						Parent
-						{mainCategories.length > 0 && (
-							<select>
-								<option>—</option>
-								{mainCategories.map((category) => (
-									<option key={category._id}>{category.title}</option>
+						<select name="parent">
+							<option value="">—</option>
+							{mainCategories.length > 0 &&
+								mainCategories.map((category) => (
+									<option key={category._id} value={category._id}>
+										{category.title}
+									</option>
 								))}
-							</select>
-						)}
+						</select>
 					</label>
 				</p>
 				<p>
-					<label>
+					<label htmlFor="color">
 						Color
-						<input type="color" />
+						<input type="color" name="color" />
 					</label>
 				</p>
 				<p>
-					<label>
+					<label htmlFor="colorBg">
 						Background Color
-						<input type="color" />
+						<input type="color" name="colorBg" />
 					</label>
 				</p>
 				<button type="submit">add category</button>
