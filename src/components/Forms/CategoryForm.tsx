adab2428636b7212841663dbaf5e1191e7081f86
fraulueneburg <github@fraulueneburg.de>
2025-08-01 13:13:32 +0200

    refactor (CategoryForm): switch from uncontrolled to controlled form + child now inherits parent colors

--- src/components/Forms/CategoryForm.tsx
+++ src/components/Forms/CategoryForm.tsx
@@ -1,17 +1,35 @@
+import { useState, useId } from 'react'
 import { useDataContext } from '../../context/Data.context'
 import { CategoryType } from '../../types'
-import CategoryTitleInput from './CategoryTitleInput'
+import ColorSelect from '../ColorSelect'
 
 export default function NewCategoryForm() {
 	const { categoryData, setCategoryData } = useDataContext()
 	const mainCategories = categoryData.data.filter((elem) => !elem.parent)
 
+	const formId = useId()
+	const [parentId, setParentId] = useState('')
+	const [title, setTitle] = useState('')
+	const [titleInvalid, setTitleInvalid] = useState(false)
+	const [color, setColor] = useState('')
+	const [colorBg, setColorBg] = useState('')
+	const hasParent = parentId !== ''
+	const parentObj = categoryData.data.find((elem) => elem._id === parentId)
+
+	const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const newTitle = event.target.value
+		const alreadyExists = categoryData.data.some((elem: CategoryType) => elem?.title === newTitle)
+
+		setTitle(newTitle)
+		setTitleInvalid(alreadyExists)
+	}
+
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault()
+
+		const newCategoryId = `c-${categoryData.data.length}`
 		const form = event.currentTarget
-		const formData = new FormData(form)
 		const invalid = form.querySelectorAll('[aria-invalid="true"]').length > 0
-		const parent = (formData.get('parent') as string) || null
 
 		if (invalid) {
 			const firstInvalid = form.querySelector('[aria-invalid="true"]')
@@ -21,61 +39,103 @@ export default function NewCategoryForm() {
 			return
 		}
 
-		const customId = `c-${categoryData.data.length}`
-		const newCategory = { _id: customId, ...Object.fromEntries(formData.entries()) } as CategoryType
+		const newCategory = {
+			_id: newCategoryId,
+			title: title,
+			...(hasParent && { parent: parentId }),
+			color: color,
+			colorBg: colorBg,
+			children: [],
+		} as CategoryType
 
 		setCategoryData((prev) => {
-			const newData = [...prev.data, newCategory]
-			if (parent) {
-				const existingChildren = prev.index[parent] ?? []
-				return {
-					data: newData,
-					index: {
-						...prev.index,
-						[parent]: [...existingChildren, customId],
-					},
-				}
-			} else {
+			const { data, index } = prev
+			const updatedData = [...data, newCategory]
+
+			if (!hasParent) {
 				return {
 					...prev,
-					data: newData,
+					data: updatedData,
 				}
+			}
+
+			const dataWithParent = updatedData.map((elem) =>
+				elem._id === parentId ? { ...elem, children: [...(elem.children ?? []), newCategoryId] } : elem
+			)
+
+			return {
+				data: dataWithParent,
+				index: {
+					...index,
+					[parentId]: [...(index[parentId] ?? []), newCategoryId],
+				},
 			}
 		})
 	}
 
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
-				<p>
-					<CategoryTitleInput />
-				</p>
-				<p>
-					<label htmlFor="cat-parent">
-						Parent
-						<select name="parent" id="cat-parent">
-							<option value="">—</option>
-							{mainCategories.length > 0 &&
-								mainCategories.map((category) => (
-									<option key={category._id} value={category._id}>
-										{category.title}
-									</option>
-								))}
-						</select>
+			<form onSubmit={handleSubmit} className="new-category">
+				{mainCategories.length > 0 && (
+					<div className="field">
+						<label>
+							Parent
+							<select name="parent" onChange={(e) => setParentId(e.target.value)}>
+								<option value="">—</option>
+								{mainCategories.length > 0 &&
+									mainCategories.map((category) => (
+										<option key={category._id} value={category._id}>
+											{category.title}
+										</option>
+									))}
+							</select>
+						</label>
+					</div>
+				)}
+
+				<div className="field">
+					<label>
+						Name
+						<input
+							type="text"
+							name="title"
+							value={title}
+							onChange={handleTitleChange}
+							aria-invalid={titleInvalid}
+							aria-errormessage={`${formId}-title-error`}
+							required
+						/>
+						{titleInvalid && (
+							<span id={`${formId}-title-error`} className="error-message">
+								Error: Category name already exists. Please choose a unique name.
+							</span>
+						)}
 					</label>
-				</p>
-				<p>
-					<label htmlFor="cat-color">
+				</div>
+				<div className="field">
+					<label>
 						Color
-						<input type="color" name="color" id="cat-color" />
+						<input
+							type="color"
+							name="color"
+							disabled={hasParent}
+							value={hasParent ? parentObj?.color : color}
+							onChange={(e) => setColor(e.target.value)}
+						/>
 					</label>
-				</p>
-				<p>
-					<label htmlFor="cat-color-bg">
+				</div>
+				<div className="field">
+					<label>
 						Background Color
-						<input type="color" name="colorBg" id="cat-color-bg" />
+						<input
+							type="color"
+							name="colorBg"
+							disabled={hasParent}
+							value={hasParent ? parentObj?.colorBg : colorBg}
+							onChange={(e) => setColorBg(e.target.value)}
+						/>
 					</label>
-				</p>
+				</div>
 				<button type="submit">add category</button>
 			</form>
 		</>
